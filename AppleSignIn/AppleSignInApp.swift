08380c8bf592813dd258ccceee51d32618bbe0ac
CSH <calum.haddow11@gmail.com>
2022-09-01 16:17:12 +0100

    Initial Commit

---
+++ AppleSignIn/AppleSignInApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSignInApp.swift
+//  AppleSignIn
+//
+//  Created by Calum Haddow on 01/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleSignInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
